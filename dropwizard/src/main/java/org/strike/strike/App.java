025b3d5fcb97c1d9c19bdd09e6e8779d8ba868b3
hector <hectorg20@gmail.com>
2014-11-21 09:58:40 -0600

    created maven project

---
+++ dropwizard/src/main/java/org/strike/strike/App.java
@@ -0,0 +1,13 @@
+package org.strike.strike;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
